a6b1a1e747dd2d8a9d9d9a115efc2008e6e2c322
Paul Summers <paulsumm@microsoft.com>
2018-09-10 21:15:31 -0400

    Please. Make. It. Work.

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import process from 'process';
 var app = express();
 
 var apiKey = process.env.API_KEY
-
+var port = normalizePort(process.env.PORT || '3000');
 let staticPath:string = path.join(__dirname, 'web')
 app.use(express.static(staticPath));
 console.log(`Static files being loaded from here: ${staticPath}`)
@@ -14,7 +14,22 @@ app.get('/', (req, res) => {
     res.send('Hello from Typescript');
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`Listening on port 3000 with API key ${apiKey}`);
 });
 
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      // named pipe
+      return val;
+    }
+  
+    if (port >= 0) {
+      // port number
+      return port;
+    }
+  
+    return false;
+  }
