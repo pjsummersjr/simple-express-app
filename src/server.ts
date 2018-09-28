e6d19c5aa4e015d9d04cad47f7ace42fece4dca9
Paul Summers <paulsumm@microsoft.com>
2018-09-28 09:46:31 -0400

    CI/CD testing

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ console.log(`Static files being loaded from here: ${staticPath}`)
 
 app.get('/api', (req, res) => {
     console.log('API called');
-    res.json({"value":[{"id":1},{"id":2}]});
+    res.json({"value":[{"id":1, "title": "Alice in Wonderland"},{"id":2, "title": "Chronicles of Narnia"}]});
 });
 
 app.listen(port, () => {
