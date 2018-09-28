43b3aaf8ddd56a2fca7a2d791aa272c5dc9e9045
Paul Summers <paulsumm@microsoft.com>
2018-09-28 11:51:30 -0400

    Added more to the JSON api endpoint

--- src/server.ts
+++ src/server.ts
@@ -12,12 +12,10 @@ console.log(`Static files being loaded from here: ${staticPath}`)
 
 app.get('/api', (req, res) => {
     console.log('API called');
-    res.json({"value":[{"id":1},{"id":2}]});
+    res.json({"value":[{"id":1, "title": "Alice in Wonderland"},{"id":2, "title": "Chronicles of Narnia"}]});
 });
 
 app.listen(port, () => {
     console.log(`Listening on port 3000 with API key ${apiKey}`);
 });
-
-//Azure AD APP ID: 4712bbca-26b2-418b-ac74-41ce06995dcb
 
